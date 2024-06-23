3686751c24c69a88e52bb592ca03371a160acda2
FelipeTMedina <felipeleonn@hotmail.com>
2024-06-22 23:23:28 -0300

    feat: add aggregate  mensage component used stylized component

--- src/styles/styled-component/components/contact1/index.jsx
+++ src/styles/styled-component/components/contact1/index.jsx
@@ -1,4 +1,4 @@
-import { Contact1Content,Contact1Link,ContactH2, Contact1Link2 } from "./style";
+import { Contact1Content,Contact1Link,ContactH2, Contact1Link2,ContactForm,ContacName,ContactInput,ContactEmail,ContactInputMsm} from "./style";
 
 export  function Contact1(){
 
@@ -9,8 +9,16 @@ export  function Contact1(){
                     <Contact1Link>Tagline</Contact1Link>
                     <ContactH2>Contact us</ContactH2>
                     <Contact1Link2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </Contact1Link2>
-
-                </Contact1Content> 
+                </Contact1Content>
+                <ContactForm>
+                    <ContacName>
+                         <ContactInput></ContactInput>
+                    </ContacName>
+                    <ContactEmail>
+                        <ContactInput></ContactInput>
+                    </ContactEmail>
+                    <ContactInputMsm></ContactInputMsm>
+                </ContactForm>
         </body>
 
     )
--- src/styles/styled-component/components/contact1/style.js
+++ src/styles/styled-component/components/contact1/style.js
@@ -5,7 +5,6 @@ export const Contact1Content = styled.div`
     display :flex;
     width: 768px;
     height: 149px;
-    border: 1px solid black;
     flex-direction: column;
 `
 
@@ -33,3 +32,42 @@ export const Contact1Link2 =styled.p`
     font-size: 16px;
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
  `
+
+export const ContactForm =styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 560px;
+    height: 553px;
+   
+`
+export const ContacName =styled.label`
+    width: 560px;
+    height: 46px;
+    border: 1px solid black;
+
+
+`
+export const ContactInput =styled.input`
+    width: 560px;
+    height: 46px;
+
+`
+
+
+export const ContactEmail =styled.label`
+    width: 560px;
+    height: 46px;
+    border: 1px solid black;
+    margin-top: 35px;
+
+`
+export const ContactInputMsm =styled.input`
+    display: flex;
+    justify-content: baseline;
+    width: 560px;
+    height: 180px;
+    border: 1px solid black;
+    margin: 24px;
+`
